901a57d41ec02d12f11776d3f38a9113d0e25f7d
prashantsagarshakya <prashant.sagarshakya@quadbtech.com>
2025-03-18 22:04:17 +0530

    Resend Updated with Zoho Mail

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -1,7 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
 
 export default async function handler(
   req: NextApiRequest,
@@ -19,25 +17,33 @@ export default async function handler(
       .json({ success: false, error: "Missing required fields" });
   }
 
-  const emailContent = `
-    Name: ${name}
-    Email: ${email}
-    Phone: ${phone || "Not Provided"}
-    Subject: ${subject}
-    Message: ${message}
-  `;
+  const transporter = nodemailer.createTransport({
+    host: "smtppro.zoho.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.ZOHO_EMAIL,
+      pass: process.env.ZOHO_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.ZOHO_EMAIL,
+    to: "prashant.sagar.shakya@gmail.com",
+    subject: subject,
+    text: `Name: ${name}\nEmail: ${email}\nPhone: ${
+      phone || "Not Provided"
+    }\nSubject: ${subject}\nMessage: ${message}`,
+  };
 
   try {
-    await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "prashant.sagarshakya@quadbtech.com",
-      subject: `${subject}`,
-      text: emailContent,
-    });
-
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: ", info.response);
     res.status(200).json({ success: true, message: "Email sent successfully" });
   } catch (error: any) {
     console.error("Failed to send email:", error);
-    res.status(500).json({ success: false, error: error.message });
+    res
+      .status(500)
+      .json({ success: false, error: error.message || "Failed to send email" });
   }
 }
