527b4d92b4d1da2750b1cfa9f1c48b37f5dc180b
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2025-04-10 07:59:52 +0530

    Apostrophy Updated Contact Section

--- src/app/components/Home/Contact.tsx
+++ src/app/components/Home/Contact.tsx
@@ -152,7 +152,7 @@ export default function ContactUs() {
       {/* Address Box */}
       <div className="absolute top-[90px] sm:top-[100px] md:top-[140px] left-1/2 transform -translate-x-1/2 bg-theme text-theme px-4 py-3 rounded-md w-[260px] sm:w-[280px] text-xs leading-5 shadow-lg z-10 text-center">
         <p className="font-bold">QB TOWER</p>
-        <p>15313 O'Connell Park,</p>
+        <p>15313 O&apos;Connell Park,</p>
         <p>Belleville 48990</p>
         <p>sales@quadbtech.com</p>
         <p>+91 8360543857</p>
