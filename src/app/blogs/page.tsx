a04714b64bbe6fca20e68498d2dffa81788d3d02
priyakumariyt05 <priya.kumari@quadbtech.com>
2025-04-16 10:58:23 +0530

    add blog mobile section

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,25 +1,48 @@
+
+"use client";
+import { useState, useEffect } from "react";
 import SectionOne from "@/app/components/blogs/sectionOne";
 import SectionTwo from "@/app/components/blogs/sectionTwo";
 import SectionThree from "@/app/components/blogs/sectionThree";
+import BlogMobile from "@/app/components/blogs/BlogMobile";
 
-export const metadata = {
-  title: "Blogs",
-};
+// export const metadata = {
+//   title: "Blogs",
+// };
 
+  
 
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 786);
+   
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
       <div>
         <main>
-          {/* Section one  */}
-          <SectionOne />
-
-          {/* Section two  */}
-          <SectionTwo />
-
-          {/* section 3 */}
-          <SectionThree />
+        {isMobile ? (
+ 
+    <BlogMobile />
+  ) : (
+  
+    <>
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+    </>
+  )}
         </main>
       </div>
   );
