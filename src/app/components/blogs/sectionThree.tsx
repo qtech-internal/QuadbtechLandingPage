35169e0116e057c2432c96ef4510ccab128de133
QuadbCompany <neeraja.naru@quadbtech.com>
2025-06-23 15:01:47 +0530

    corrected blogs images changes according to theme

--- src/app/components/blogs/sectionThree.tsx
+++ src/app/components/blogs/sectionThree.tsx
@@ -3,14 +3,14 @@
 import Image from "next/image";
 
 
-export default function SectionThree() {
+export default function SectionThree({ image1,image2 }: { image1: string,image2:string }) {
 
 
 
     return (
         <section  className="w-full py-12 px-4 md:px-6 lg:px-8">
             <div className="container relative flex flex-col md:flex-row max-auto max-w-8xl gap-8">
-                <div className="relative w-full md:w-[40%] aspect-[16/16] md:aspect-[4/3] overflow-hidden">
+                <div className="relative w-full md:w-[40%] aspect-[16/16] md:aspect-[4/3] ">
                     <div
 
                         className="
@@ -21,7 +21,7 @@ export default function SectionThree() {
               text-[5vw] sm:text-[4.7vw] md:text-[14px] font-semibold
               md:leading-6.5 lg:leading-9.5 lg:text-[20px]
               xl:text-[26px] xl:leading-13
-              2xl:text-[32px] 2xl:leading-16
+              2xl:text-[32px] 2xl:leading-14
               sm:leading-12 !text-yellow-950
               leading-10
               px-4 sm:p-6 rounded-lg transition-all duration-300 [word-spacing:0.2rem]"
@@ -32,13 +32,13 @@ export default function SectionThree() {
                         Workspaces
                     </div>
 
-                    <button className="absolute cursor-pointer item center ml-3 md:text-[1vw] 2xl:text-[18px]  border-2 border-orange-500 rounded-2xl bottom-1 z-10 text-black bg-white left-45 translate-x-[-20px] md:left-25 lg:left-35 xl:left-45 2xl:left-55 px-2 py-1  ">
+                    <button className="absolute cursor-pointer item center ml-3 md:text-[1vw] 2xl:text-[18px] border border-theme rounded-2xl -bottom-5 z-10 text-black bg-white left-45 translate-x-[-20px] md:left-25 lg:left-35 xl:left-45 2xl:left-53 px-2 py-1   ">
                         Read More
                     </button>
 
                     <Image
                         draggable={false}
-                         src="/Frame 2134280867.png"
+                        src={image1}
                         alt="section_3_Image"
                         objectFit="cover"
                         className="rounded-2xl transition-all duration-300 translate-y-[-10px] rounded-bottom-2xl  "
@@ -47,7 +47,7 @@ export default function SectionThree() {
                 </div>
 
                 <div className="relative w-full md:w-[60%] aspect-[16/16] md:aspect-[4/3] overflow-hidden ">
-                    <div className="absolute top-[20%]    font-bold text-[4vw] sm:text-[3.5vw] md:text-[18px] md:leading-10 lg:text-[24px] 2xl:text-[45px] lg:leading-14 2xl:leading-20 xl:text-[2.5vw] xl:leading-18 leading-14 transition-all duration-300 [word-spacing:0.2rem]">
+                    <div className="absolute top-[20%]  font-bold text-[4vw] sm:text-[3.5vw] md:text-[18px] md:leading-10 lg:text-[24px] 2xl:text-[45px] lg:leading-14 2xl:leading-20 xl:text-[2.5vw] xl:leading-18 leading-14 transition-all duration-300 ">
                         <span>Full-Stack</span>
                         <br />
                         <span>Development in 2024:</span>
@@ -58,18 +58,18 @@ export default function SectionThree() {
 
                     </div>
 
-                    <button className="absolute cursor-pointer md:text-[1vw] border-2 border-orange-500 rounded-2xl top-10 z-10 left-2 bg-white md:top-5 lg:top-8 xl:top-12 md:left-5  px-4 py-1 2xl:text-[18px] ">
+                    <button className="absolute cursor-pointer md:text-[1vw] border border-theme rounded-2xl top-10 z-10 left-2 bg-white md:top-5 lg:top-8 xl:top-12 md:left-5  px-4 py-1 2xl:text-[18px] ">
                         Next Gen
                     </button>
 
-                    <button className="absolute cursor-pointer left-2 bottom-20 md:text-[1vw] border-2 font-bold border-orange-500 rounded-2xl z-10 text-white bg-orange-400  md:left-5 md:bottom-13 lg:bottom-18 xl:bottom-22 2xl:bottom-28 px-2 py-1 2xl:text-[18px]">
+                    <button className="absolute cursor-pointer left-2 bottom-20 md:text-[1vw]  font-bold  rounded-2xl z-10 text-white bg-theme  md:left-5 md:bottom-13 lg:bottom-18 xl:bottom-22 2xl:bottom-28 px-4 py-2 2xl:text-[18px]">
                         READ MORE
                     </button>
    {/* <div className="absolute inset-0 bg-[var(--bg-card)] opacity-50"></div> */}
                    <div className="relative w-[690px] h-[640px] ml-7 ">
                      <Image
                         draggable={false}
-                        src="/cir.png"
+                        src={image2}
                         alt="section_3_image2"
                         layout="fill"
                         objectFit="cover"
